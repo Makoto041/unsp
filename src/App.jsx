a0dd52898bd2f8d47022f7427b9425a612314af5
Makoto Iwabuchi <makoto01401@gmail.com>
2024-11-30 18:31:39 +0900

    add: getImage func

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ function App() {
   const [word, setWord] = useState("")
 
   // Unsplashからデータを受け取る関数
-  
+  const getPhotoData = () => {
+    axios.get(
+      "https://api.unsplash.com/search/photos?query=cat&client_id=3upG4lhXeUhpg5cynzAA2iAeBfhqe_OA-1Njox0krQQ")
+        .then((res) => console.log(res));
+  };
   return (
     <div className="App">
       <Title />
