96d7029805a18d9b4a5fbe995401fa2c1e47ca9f
Makoto Iwabuchi <makoto01401@gmail.com>
2024-12-01 14:49:47 +0900

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/unsp/',
-})
+  build: {
+    cssCodeSplit: true, // CSS ファイルを分割する設定
+  },
+  base: "/unsp/",
+});
